188b46694db8e9d502ff121091861dc5fd1c007c
Chudi Ifeanyi Onyilimba <onyilimba@gmail.com>
2015-09-19 08:18:13 -0400

    third commit

--- livelab.php
+++ livelab.php
@@ -3,7 +3,7 @@ require 'vendor/autoload.php';
 use Aws\S3\S3Client;
 $client = S3Client::factory();
 
-$bucket = "acloudguru-phps3bucket";
+$bucket = "bluemetrix";
 echo "Creating bucket named {$bucket}\n";
 $result = $client->createBucket(array(
     'Bucket' => $bucket
